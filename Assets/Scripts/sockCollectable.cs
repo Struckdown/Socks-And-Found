96753e643b741afe3170156c65c32789bc8cecdd
Zeyu-Li <zeyu.li23@gmail.com>
2021-01-30 13:40:06 -0600

    added sock collectable

---
+++ Assets/Scripts/sockCollectable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sockCollectable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int value = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            collectablesManager.instance.changeScore(value);
+            Destroy(gameObject);
+        }
+    }
+}
