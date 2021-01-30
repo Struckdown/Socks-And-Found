97a00740527288e3e154fec689ce4b0446f9ddc9
Struckdown <bfleysher@gmail.com>
2021-01-30 01:08:54 -0700

    Initial walking animation and sock shooting

---
+++ Assets/Scripts/FlyingSock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingSock : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float flyspeed = 3;
+    public Vector2 moveDirection = new Vector2(1, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = moveDirection.normalized * Time.deltaTime * flyspeed;
+    }
+}
--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -14,6 +14,11 @@ public class movement : MonoBehaviour
     // inits rigid body
     private Rigidbody2D rb;
 
+    public Animator animator;
+    //public GameObject sock;
+    public int flyingSocksLeft = 10;
+    private Vector2 shootDir;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +28,7 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
         // flips player
         // if (moveInput > 0) {
         //     transform.eulerAngles = new Vector3(0, 0, 0);
@@ -32,16 +37,29 @@ public class movement : MonoBehaviour
         //     transform.eulerAngles = new Vector3(0, 180, 0);
         // }
 
+        if (Input.GetButtonDown("Fire1"))
+        {
+            shootDir = Input.mousePosition - transform.position;
+            GameObject sock = (GameObject)Instantiate(Resources.Load("Prefabs/FlyingSock"));
+            sock.transform.position = transform.position;
+            //sock.moveDirection = Vector2(1, 1);
+            
+        }
+
         // quits game if escape button is entered
         if (Input.GetKey("escape")) {
             Application.Quit();
         }
     }
     void FixedUpdate() {
-        // horizontal movement
+        // movement
         moveInputHorizontal = Input.GetAxisRaw("Horizontal");
         moveInputVertical = Input.GetAxisRaw("Vertical");
-        // Debug.Log(moveInputVertical);
+
+        animator.SetFloat("HorizontalMovement", moveInputHorizontal);
+        animator.SetFloat("VerticalMovement", moveInputVertical);
+        
+
         rb.velocity = new Vector2(moveInputHorizontal * speed, moveInputVertical * speed);
     }
 }
