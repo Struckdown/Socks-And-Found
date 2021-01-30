e1b68266052af3e83cc2b54968b901021f3ea8a4
Zeyu-Li <zeyu.li23@gmail.com>
2021-01-30 14:24:48 -0600

    added health bar

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public int currentHealth;
+    public int maxHealth = 20;
+    public HealthBarScript healthBar;
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L)) {
+            TakeDamage(1);
+        }
+        
+    }
+
+    void TakeDamage(int damage) {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
