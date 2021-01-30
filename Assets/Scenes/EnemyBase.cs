c477bb2f577ae086eb98107d742b88edacd8df08
Struckdown <bfleysher@gmail.com>
2021-01-29 19:46:59 -0700

    Reordered git heiarchy, began work on enemy script

---
+++ Assets/Scenes/EnemyBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    private GameObject player;
+    public int moveSpeed = 10;
+    public Vector2 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (true) {//((player.transform.position-this.transform.position).sqrMagnitude<3*3) {
+            // the player is within a radius of 3 units to this enemy
+
+            // moveDirection = player.transform.position - this.transform.position;
+            //moveDirection = moveDirection.forward * Time.deltaTime;
+            transform.Translate(moveDirection.normalized * Time.deltaTime * moveSpeed);
+        }
+    }
+
+}
