d11553c279302028d982d6860b3852aedd170fd9
Struckdown <bfleysher@gmail.com>
2021-01-31 00:12:19 -0700

    Audio for footsteps and sock shots

--- Assets/Scripts/FlyingSock.cs
+++ Assets/Scripts/FlyingSock.cs
@@ -7,11 +7,15 @@ public class FlyingSock : MonoBehaviour
     private Rigidbody2D rb;
     public float flyspeed = 15;
     public Vector2 moveDirection = new Vector2(-1, 0);
+    public AudioClip[] audio;
+    public AudioSource randomSound;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        randomSound.clip = audio[Random.Range(0, audio.Length-1)];
+        randomSound.Play();
     }
 
     void FixedUpdate()
