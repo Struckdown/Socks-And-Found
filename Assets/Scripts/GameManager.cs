7d653c2b0e9912995abcabeb22194ec3f70444af
Struckdown <bfleysher@gmail.com>
2021-01-30 19:54:13 -0700

    Scene transitions WIP

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameManager
+{
+    public static string scene;
+
+    public static void changeScene(string s)
+    {
+        return;
+    }
+}
