b160e1ca3844a7ddc4958bd528d3fa9a7c2639da
Struckdown <bfleysher@gmail.com>
2021-01-30 20:16:14 -0700

    Scene transitions with doors WIP ongoing

---
+++ Assets/Scripts/LevelTransition.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour
+{
+
+    public Animator transition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadNextLevel(string stringLevelName)
+    {
+        StartCoroutine(LoadLevel(stringLevelName));
+    }
+
+    IEnumerator LoadLevel(string stringLevelName)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(stringLevelName);
+    }
+}
