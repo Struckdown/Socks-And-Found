d0095038d0e86889495595910d3650459a8a04cb
Zeyu-Li <zeyu.li23@gmail.com>
2021-01-31 12:57:38 -0600

    added most UI elements

--- Assets/Scripts/healthManager.cs
+++ Assets/Scripts/healthManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class healthManager : MonoBehaviour
 {
     public int currentHealth;
     public int maxHealth = 20;
