d2e5d694f5a311fe714528d13aefc0770564c5a3
Zeyu-Li <zeyu.li23@gmail.com>
2021-01-31 13:35:46 -0600

    finished sounds

--- Assets/Scripts/collectablesManager.cs
+++ Assets/Scripts/collectablesManager.cs
@@ -10,6 +10,9 @@ public class collectablesManager : MonoBehaviour
     public TextMeshProUGUI textDirty;
     public int cleanSocks = 0;
     public int dirtySocks = 0;
+    public AudioSource audioSourceDirty;
+    public AudioSource audioSourceClean;
+    public AudioSource audioSourceCleanSocks;
 
     // Start is called before the first frame update
     void Start()
@@ -21,14 +24,17 @@ public class collectablesManager : MonoBehaviour
 
     // Update is called once per frame
     public void changeCleanSocks(int sockValue) {
+        audioSourceClean.Play();
         cleanSocks += sockValue;
         textClean.text = "x" + cleanSocks.ToString();
     }
     public void changeDirtySocks(int sockValue) {
+        audioSourceDirty.Play();
         dirtySocks += sockValue;
         textDirty.text = "x" + dirtySocks.ToString();
     }
     public void cleanDirtySocks() {
+        if (dirtySocks != 0) audioSourceCleanSocks.Play();
         cleanSocks += dirtySocks;
         dirtySocks = 0;
         // updates text
